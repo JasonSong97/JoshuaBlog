35c22ca97c8d81c57676288c299ad55042c91a7d
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-04-19 21:35:39 +0900

    security complete

---
+++ src/main/java/pastry/coffeecoding/joshuablog/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package pastry.coffeecoding.joshuablog.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean // 권한 주소 설정
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 1. CSRF 해제
+        http.csrf().disable();
+
+        // 2. Form 로그인 설정
+        http.formLogin()
+                .loginPage("/loginForm") // 리다이렉션
+                .loginProcessingUrl("/login")
+                .successHandler(((request, response, authentication) -> {
+                    log.debug("디버그 : 로그인 성공");
+                }))
+                .failureHandler(((request, response, exception) -> { // 인증과 권한 실패시 (엔트리)
+                    log.debug("디버그 : 로그인 실패 : " + exception.getMessage());
+                }));
+
+
+        // 3. 인증, 권한 필터 설정 -> 권한은 X
+        http.authorizeRequests(
+                authorize -> authorize.antMatchers("/s/**").authenticated() // 인증
+                        .anyRequest().permitAll()
+        );
+
+        return http.build();
+    }
+}
