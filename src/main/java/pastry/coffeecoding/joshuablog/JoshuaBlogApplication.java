f3979567dd946f19ebcf30fb8b8e26dc06472501
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-04-19 21:01:54 +0900

    feat: add setting

---
+++ src/main/java/pastry/coffeecoding/joshuablog/JoshuaBlogApplication.java
@@ -0,0 +1,13 @@
+package pastry.coffeecoding.joshuablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoshuaBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoshuaBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pastry/coffeecoding/joshuablog/JoshuaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package pastry.coffeecoding.joshuablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoshuaBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
